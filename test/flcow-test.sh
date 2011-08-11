d3682bc283bd0e84b8c89d77d9466fbc6dadd5cc
Jonathan <thegoldenbee@gmail.com>
2011-08-11 10:40:36 -0400

    added dirname to avoid test confusion

--- test/flcow-test.sh
+++ test/flcow-test.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
-mac_lib=$(pwd)/../fl-cow/.libs/libflcow.dylib
-lnx_lib=$(pwd)/../fl-cow/.libs/libflcow.so
+mac_lib=`dirname $0`/../fl-cow/.libs/libflcow.dylib
+lnx_lib=`dirname $0`/../fl-cow/.libs/libflcow.so
 
 export DYLD_INSERT_LIBRARIES=${mac_lib}
 export DYLD_FORCE_FLAT_NAMESPACE=1
 export LD_PRELOAD=${lnx_lib}
 
-./flcow-test
+`dirname $0`/flcow-test
 
 
 
